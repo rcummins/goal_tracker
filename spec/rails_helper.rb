1ac17aeebf6bec05bad2586917778f86957de382
Renata Cummins <renatacummins@gmail.com>
2019-07-08 12:23:50 -0400

    fix: typo in configuration for shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ RSpec.configure do |config|
 end
 
 # Configure shoulda matchers
-Shoulda::Matchers.configure.do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
