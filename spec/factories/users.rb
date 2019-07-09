1b2ee5ab7fdae9a67f6bc15450f39fce1fc3ba55
Renata Cummins <renatacummins@gmail.com>
2019-07-09 17:51:04 -0400

    fix: add minimum password length of 8

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     username { Faker::Internet.username }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(8) }
   end
 end
