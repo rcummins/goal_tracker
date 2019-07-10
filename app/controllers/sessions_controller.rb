168d4dda45382004d8d96fe8cfc6834438409c93
Renata Cummins <renatacummins@gmail.com>
2019-07-10 10:30:44 -0400

    fix: put error message in array for use with 'each' iterator

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       login_user!(@user)
       redirect_to user_url(@user)
     else
-      flash.now[:errors] = "Incorrect username and/or password"
+      flash.now[:errors] = ["Incorrect username and/or password"]
       render :new
     end
   end
