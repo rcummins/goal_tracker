5d9ccd17fa9b990ecad771cd4e43668c3b56ef51
Renata Cummins <renatacummins@gmail.com>
2019-10-29 20:10:18 -0400

    fix: clear signup errors after redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
       login_user!(@user)
       redirect_to user_url(@user)
     else
-      flash[:errors] = @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
